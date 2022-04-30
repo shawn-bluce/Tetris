f1fb073af0405e28b239d7c7f65d1c66e4573215
shawn <shawnbluce@gmail.com>
2022-04-30 16:38:05 +0800

    feature: any type block, bug no rotate

--- graphics/tetrisBlock.go
+++ graphics/tetrisBlock.go
@@ -24,14 +24,58 @@ var colorList [9]color.RGBA = [9]color.RGBA{
 	colornames.Peru, colornames.Orange, colornames.Blueviolet,
 }
 
+var blockList [7]string = [7]string{"S", "Z", "RZ", "L", "RL", "T", "I"}
+
 func GenerateNewBlock() Block {
 	thisColor := colorList[rand.Int()%9]
+	thisBlockName := blockList[rand.Int()%7]
 	block := Block{
 		Color: thisColor,
 	}
-	block.BlockList[0] = SubBlock{125, 5, true, thisColor}
-	block.BlockList[1] = SubBlock{155, 5, true, thisColor}
-	block.BlockList[2] = SubBlock{125, 35, true, thisColor}
-	block.BlockList[3] = SubBlock{155, 35, true, thisColor}
+
+	blockList := [4][2]float64{}
+
+	switch thisBlockName {
+	case "S":
+		blockList[0] = [2]float64{0, 0}
+		blockList[1] = [2]float64{1, 0}
+		blockList[2] = [2]float64{0, 1}
+		blockList[3] = [2]float64{1, 1}
+	case "Z":
+		blockList[0] = [2]float64{0, 0}
+		blockList[1] = [2]float64{1, 0}
+		blockList[2] = [2]float64{1, 1}
+		blockList[3] = [2]float64{2, 1}
+	case "RZ":
+		blockList[0] = [2]float64{1, 0}
+		blockList[1] = [2]float64{2, 0}
+		blockList[2] = [2]float64{0, 1}
+		blockList[3] = [2]float64{1, 1}
+	case "L":
+		blockList[0] = [2]float64{0, 0}
+		blockList[1] = [2]float64{0, 1}
+		blockList[2] = [2]float64{0, 2}
+		blockList[3] = [2]float64{1, 2}
+	case "RL":
+		blockList[0] = [2]float64{1, 0}
+		blockList[1] = [2]float64{1, 1}
+		blockList[2] = [2]float64{2, 0}
+		blockList[3] = [2]float64{2, 1}
+	case "T":
+		blockList[0] = [2]float64{1, 0}
+		blockList[1] = [2]float64{0, 1}
+		blockList[2] = [2]float64{1, 1}
+		blockList[3] = [2]float64{2, 1}
+	case "I":
+		blockList[0] = [2]float64{0, 0}
+		blockList[1] = [2]float64{1, 0}
+		blockList[2] = [2]float64{2, 0}
+		blockList[3] = [2]float64{3, 0}
+	}
+
+	block.BlockList[0] = SubBlock{(blockList[0][0]+4)*30 + 5, blockList[0][1]*30 + 5, true, thisColor}
+	block.BlockList[1] = SubBlock{(blockList[1][0]+4)*30 + 5, blockList[1][1]*30 + 5, true, thisColor}
+	block.BlockList[2] = SubBlock{(blockList[2][0]+4)*30 + 5, blockList[2][1]*30 + 5, true, thisColor}
+	block.BlockList[3] = SubBlock{(blockList[3][0]+4)*30 + 5, blockList[3][1]*30 + 5, true, thisColor}
 	return block
 }
